b4426751c2ff05fb6dbf40b4d84f85ac7f668ca5
Vladislav <112154695+Vlad-Sharapov@users.noreply.github.com>
2023-07-26 22:28:35 +0300

    fix: fix style

--- stat-service/stat-server/src/main/java/ru/yandex/practicum/statservice/dto/StatMapper.java
+++ stat-service/stat-server/src/main/java/ru/yandex/practicum/statservice/dto/StatMapper.java
@@ -14,6 +14,8 @@ import java.util.stream.Collectors;
 public class StatMapper {
 
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
     public static HitDto toHitDto(Hit hit) {
         String timestamp = hit.getTime().format(formatter);
 
